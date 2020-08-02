ba953568bb91930a3b45a0e541c88292f4956a7f
Shivam Sukhwal <49242766+ShivamSukhwal@users.noreply.github.com>
2020-08-02 12:36:07 -0400

    made entries not visable

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const $entryForm = document.querySelector("#entryForm");
 const $entries = document.getElementById("entries");
 const $entryTextBox = document.querySelector(".entry-textbox");
+const $entriesNav = document.querySelector(".entries-nav");
 
+let count = 1;
 const addEntryToDom = (event) => {
   event.preventDefault();
-  const submittedEntry = $entryTextBox.value;
-  $entries.innerHTML += `<div class='entry'> ${submittedEntry}</div>`;
+
+  $entries.innerHTML += `<div class='entry'> ${$entryTextBox.value}</div>`;
+  const $entry = document.getElementsByClassName("entry");
+  $entry.innerText = $entryTextBox.value;
+  $entryTextBox.value = "";
+  $entry[count - 1].style.display = "none";
+
+  $entriesNav.innerHTML += `<button class='entries-button'>${count}</button>`;
+  count++;
 };
 
 $entryForm.addEventListener("submit", addEntryToDom);
